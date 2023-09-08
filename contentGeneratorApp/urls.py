914cf8334949118cfac6055a14fb8641c03a4b34
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-09 01:19:27 +0300

    about us page

--- contentGeneratorApp/urls.py
+++ contentGeneratorApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about-aggie-ai', views.about, name='about')
 ]   
--- contentGeneratorApp/views.py
+++ contentGeneratorApp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render (request, 'landing/index.html', {})
+    return render (request, 'layout/landing.html', {})
+
+
+def about(request):
+    return render(request, 'landing/about.html', {})
