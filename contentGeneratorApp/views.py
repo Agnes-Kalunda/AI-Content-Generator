ae9f5f86d0a307c9d4238b03b54529d635befa8e
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-09 01:35:09 +0300

    debugged

--- contentGeneratorApp/views.py
+++ contentGeneratorApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render (request, 'layout/landing.html', {})
+    return render (request, 'landing/index.html', {})
 
 
 def about(request):
