66b0e3bab5fab1ebe885906d1f4ed80da59804af
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-09 18:20:02 +0300

    password auth

--- contentGeneratorApp/views.py
+++ contentGeneratorApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages 
 # Create your views here.
 def home(request):
     return render (request, 'landing/index.html', {})
@@ -13,4 +14,15 @@ def login(request):
 
 
 def register(request):
+   
+    if request.method == 'POST':
+        email= request.POST['email'].replace('','').lower()
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if not password1 == password2:
+            messages.error(request,'Passwords do not match')
+       
+
+        return redirect('register')
     return render(request, 'authorization/register.html', {})
