9f95a1f8117cf35cc87e88b789df7eb273d2ae55
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-12 00:21:43 +0300

    refactored

--- contentGeneratorApp/forms.py
+++ contentGeneratorApp/forms.py
@@ -20,9 +20,18 @@ city=forms.CharField(
         required=True,
         label='City',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter City'}),)
+
 province= forms.CharField(
         required=True,
         label='Province',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Province'}),)
-country=
-postalCode=
+
+country=forms.CharField(
+        required=True,
+        label='Country',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Country'}),)
+
+postalCode=forms.CharField(
+        required=True,
+        label='Postal Code',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Postal Code'}),)
