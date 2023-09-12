9cc932ab71d99bb4f432bfabf4c03ac8c4e08869
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-13 00:29:24 +0300

    json response

--- contentGeneratorApp/functions.py
+++ contentGeneratorApp/functions.py
@@ -4,13 +4,14 @@ from django.conf import settings
 
 openai.api_key = settings.OPENAI_API_KEYS
 
-def generateBlogTopicIdeas():
+def generateBlogTopicIdeas(topic, keywords):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt='Generate blog topic ideas on cryptocurrency',
+        prompt='Generate blog topic ideeas on the following topic: {} \n'.format(topic, keywords),
         temperature=0.7,
         max_tokens=250,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0
-)
+        presence_penalty=0)
+    
+    return response.choices[0].text
