79acdb22f7e75181277fdfb022a36dc9e22c5d8a
Agnes-Kalunda <agypeter97@gmail.com>
2023-09-12 03:15:25 +0300

    profile photo avatar

--- contentGeneratorApp/urls.py
+++ contentGeneratorApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.conf import settings
 
 
 
@@ -14,3 +15,6 @@ urlpatterns = [
 
 
 ]   
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
