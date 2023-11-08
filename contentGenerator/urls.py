9ae0d884c61a4305149aba5e25c05bac78296d32
Agnes-Kalunda <agypeter97@gmail.com>
2023-11-08 16:17:14 +0300

    avatar switch

--- contentGenerator/urls.py
+++ contentGenerator/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contentGeneratorApp.urls')),
-    path('auth/', include('contentGeneratorApp.urls')),
+    path('auth', include('contentGeneratorApp.urls')),
 ]   
 
 
